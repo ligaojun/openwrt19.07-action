245a2903f1a94a64630c5d15161de1d698a88a5c
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-10-28 13:08:27 +0800

    rewrite theme link

--- n3diy.sh
+++ n3diy.sh
@@ -9,4 +9,4 @@ rm -rf lede/package/lean/luci-theme-argon
 #添加额外软件包
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
 git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
-git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
