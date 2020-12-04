8ead9543254406e5e8c26f87c8de4e5a25878c64
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-12-04 11:47:42 +0800

    Update k3-part2.sh

--- k3-part2.sh
+++ k3-part2.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 #Set temperature display
-sed -i '/<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></td></tr> <tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%></td></tr>' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i '/Kernel Version/a\<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%></td></tr>' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 #移除无用软件包
 rm -rf k3screenctrl package/lean
